696338315c85f13bb32ed63b048c031bcf9377a7
Natan <natanhsanches@gmail.com>
2021-05-09 18:39:25 -0300

    Initial commit

---
+++ Projeto-1/include/readline.h
@@ -0,0 +1,9 @@
+#ifndef _READLINE_H_
+#define _READLINE_H_
+
+    #include <stdio.h>
+    #include <stdlib.h>
+
+    char *readline(FILE *stream);
+    
+#endif
---
+++ Projeto-1/src/readline.c
@@ -0,0 +1,31 @@
+#include "readline.h"
+
+#define READLINE_BUFFER 4096
+
+// Reads a string until '\n' or end of file
+// - Return value: Read string from stream (char *)
+char *readline(FILE *stream) {
+    char ch;
+    do {
+        ch = fgetc(stream);
+    } while (ch == '\n');
+
+    char *string = NULL;
+    int pos = 0;
+    do {
+        if (pos != 0) 
+            ch = fgetc(stream);
+        if (ch != '\r' && ch != '\n') {
+           if (pos % READLINE_BUFFER == 0) {
+                string = (char *) realloc(string, 1+(pos/READLINE_BUFFER + 1)*READLINE_BUFFER);
+            }
+            string[pos++] = ch;
+        }
+    } while (ch != '\n' && ch != EOF);
+    if (pos == 0)
+        return NULL;
+    string[pos] = '\0';
+    string = (char *) realloc(string, pos+1);
+    return string;
+}
+
