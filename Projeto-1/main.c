765d9638aa6b0928034c2aec8a145ecfd839a672
Natan <natanhsanches@gmail.com>
2021-05-21 19:49:01 -0300

    Finished funcionality 6

--- Projeto-1/main.c
+++ Projeto-1/main.c
@@ -5,6 +5,10 @@
 #include "VeiculoFileManager.h"
 #include "Utility.h"
 
+#define INTEGER_TYPE 'I'
+#define CHAR_TYPE 'C'
+#define STRING_TYPE 'S'
+
 #define DELIM " "
 #define LINE_BREAK "\n"
 #define ERROR "Falha no processamento do arquivo."
@@ -14,6 +18,7 @@
 #define WRITE_LINHA_BINARY '2'
 #define SHOW_VEICULO_CONTENT '3'
 #define SHOW_LINHA_CONTENT '4'
+#define SEARCH_LINHA_CONTENT '6'
 
 int main(int argc, char *argv[]) {
     char *inputLine = readline(stdin);
@@ -63,6 +68,76 @@ int main(int argc, char *argv[]) {
                 freeLinhaHeader(&fileHeader);
                 fclose(binFile);
                 break; 
+            }
+            case SEARCH_LINHA_CONTENT: {
+                char *binaryFilename = strsep(&inputLine, DELIM);
+                char *fieldName = strsep(&inputLine, DELIM);
+                char *fieldValue = NULL;
+                
+                LinhaData *newRegistry = (LinhaData *) malloc(sizeof *newRegistry);
+
+                void *searchedField = NULL;
+                int32_t *fieldSize = NULL;
+                char fieldType;
+                if (!strcmp(fieldName, "codLinha")) {
+                    fieldValue = strsep(&(inputLine), LINE_BREAK);
+                    searchedField = (int32_t *) &newRegistry->linhaCode;
+                    fieldType = INTEGER_TYPE;
+                }
+                else if (!strcmp(fieldName, "aceitaCartao")) {
+                    ++inputLine;
+                    fieldValue = strsep(&(inputLine), "\"");
+                    searchedField = (char *) &newRegistry->cardAcceptance;
+                    fieldType = CHAR_TYPE;
+                }
+                else if (!strcmp(fieldName, "nomeLinha")) {
+                    ++inputLine;
+                    fieldValue = strsep(&(inputLine), "\"");
+                    searchedField = (char **) &newRegistry->linhaName;
+                    fieldSize = &newRegistry->nameSize;
+                    fieldType = STRING_TYPE;
+                }
+                else if (!strcmp(fieldName, "corLinha")) {
+                    ++inputLine;
+                    fieldValue = strsep(&(inputLine), "\"");
+                    searchedField = (char **) &newRegistry->linhaColor;
+                    fieldSize = &newRegistry->colorSize;
+                    fieldType = STRING_TYPE;
+                }
+
+                FILE *binFile = fopen(binaryFilename, "rb");
+                if (!binFile) {
+                    printf("%s\n", ERROR);
+                    return 1;
+                }
+
+                LinhaHeader *fileHeader = loadLinhaBinaryHeader(binFile);
+                while (loadLinhaBinaryRegistry(binFile, newRegistry)) {
+                    if (newRegistry->isRemoved == VALID_REGISTRY) {
+                        switch (fieldType) {
+                            case INTEGER_TYPE : {
+                                if (*((int32_t *) searchedField) == atoi(fieldValue))
+                                    printLinhaRegistry(fileHeader, newRegistry);
+                                break;
+                            }
+                            case CHAR_TYPE : {
+                                if (*((char *) searchedField) == fieldValue[0]) 
+                                    printLinhaRegistry(fileHeader, newRegistry);
+                                break;
+                            }
+                            case STRING_TYPE : {
+                                if (!strncmp(*(char **) searchedField, fieldValue, *fieldSize))
+                                    printLinhaRegistry(fileHeader, newRegistry);
+                                break;
+                            }
+                        }
+                        freeLinhaData(newRegistry);
+                    }
+                }
+                free(newRegistry);
+                freeLinhaHeader(&fileHeader);
+                fclose(binFile);
+                break;
             }
         }
     }
