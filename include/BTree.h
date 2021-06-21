fe0a21a63012dc2fc5c42c0df3a8fc820d24069d
Natan <natanhsanches@gmail.com>
2021-06-21 19:46:47 -0300

    Remade project root directory structure

---
+++ include/BTree.h
@@ -0,0 +1,33 @@
+#ifndef _BTREE_H_
+#define _BTREE_H_
+
+    #include <stdlib.h>
+    #include <stdio.h>
+    #include <stdbool.h>
+    #include <stdint.h>
+
+    #define DISK_PAGE_SIZE 77
+    #define BTREE_ORDER 5
+
+    // Header liquid size = 9 bytes
+    typedef struct _BTreeHeader {
+        char fileStatus;
+        int32_t rootNode;
+        int32_t nextNodeRRN;
+    } BTreeHeader;
+
+    // BTree data registry liquid size = DISK_PAGE_SIZE
+    typedef struct _IndexStruct {
+        int32_t key;
+        int64_t regOffset;
+    } IndexStruct;
+
+    typedef struct _BTreeNode {
+        char isLeaf;
+        int32_t keyCounter;
+        int32_t nodeRRN;
+        int32_t childPointers[5];
+        IndexStruct keyValues[4];
+    } BTreeNode;
+
+#endif
